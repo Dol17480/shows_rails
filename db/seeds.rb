9b6ebd7e9ccc9d62eaf42027e743d923127be4c0
David O'Leary <david.oleary174@gmail.com>
2017-01-16 17:16:26 +0000

    files setup and working

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+Show.delete_all
+
+show = Show.create({title: "Fargo", series:2, description: "Fargo is an American black comedy–crime drama anthology television series created and primarily written by Noah Hawley. ", image: "http://deadshirt.net/wp-content/uploads/2015/12/fargo.jpg", programmeID:001})
+
+
+
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
